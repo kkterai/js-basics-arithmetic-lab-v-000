13dc08f3f89223864a3ab91dad6e87241badc483
Kenlyn T <kkterai@gmail.com>
2017-12-14 07:30:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,6 @@ const newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(currentAge);
 
 const randomNumber = Math.floor(Math.random() * 10);
+
+const randomInteger = Math.floor(randomNumber);
+
